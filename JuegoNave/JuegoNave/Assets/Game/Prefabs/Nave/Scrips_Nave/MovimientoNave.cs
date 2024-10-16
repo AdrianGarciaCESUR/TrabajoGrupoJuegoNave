65d75085e75029405292be9717aa92e332a0d2f9
josema002 <josemanuel.s360563@cesurformacion.com>
2024-10-16 14:37:29 +0200

    nave actualizado
    script nave actualizado

--- JuegoNave/JuegoNave/Assets/Game/Prefabs/Nave/Scrips_Nave/MovimientoNave.cs
+++ JuegoNave/JuegoNave/Assets/Game/Prefabs/Nave/Scrips_Nave/MovimientoNave.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class MovimientoNave : MonoBehaviour
 {
     public float speed = 3f;
+    public float desaceleracion = 3f;
     public Rigidbody2D _rb;
     float _move;
     Vector2 starPosition;
-
+    Vector2 fuerza;
+    Vector2 fuerzaOpo;
+    bool seMueve = false;
 
     void Start()
     {
@@ -18,8 +21,28 @@ public class MovimientoNave : MonoBehaviour
 
     void Update()
     {
-            _move = Input.GetAxisRaw("Horizontal_suave");
-            _rb.velocity = new Vector2(_move * speed * Time.deltaTime, _rb.velocity.y);
+        _move = Input.GetAxisRaw("Horizontal_suave");
+        fuerza = new Vector2(_move * speed * Time.deltaTime, 0);//uhoiirewuhoi
+        fuerzaOpo = new Vector2(1 * desaceleracion * Time.deltaTime, 0);
+
+        _rb.AddForce(fuerza, ForceMode2D.Impulse);
+
+        //_rb.velocity = new Vector2(_move * speed * Time.deltaTime, _rb.velocity.y);
+        
+        
+        if (Input.GetAxisRaw("Horizontal_suave") == 1 && _rb.velocity != Vector2.zero)
+        {
+            _rb.AddForce(-fuerzaOpo, ForceMode2D.Impulse);
+        }
+        else if(Input.GetAxisRaw("Horizontal_suave") == -1 && _rb.velocity != Vector2.zero)
+        {
+            _rb.AddForce(fuerzaOpo, ForceMode2D.Impulse);
+        }
+        else if(Input.GetAxisRaw("Horizontal_suave") == 0)
+        {
+            _rb.AddForce(fuerza , ForceMode2D.Impulse);
+            _rb.AddForce(-fuerza , ForceMode2D.Impulse);
+        }
     }
     public void Reset()
     {
