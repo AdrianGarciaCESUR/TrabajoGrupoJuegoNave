082b49282608e74f7a3b10b90bf72c0a3487f86f
AbelMolinaCesur <abel.s255606@cesurformacion.com>
2024-10-17 12:36:35 +0200

    Pausa, QuitApplication

--- JuegoNave/JuegoNave/Assets/Game/Scripts/UI_Manager.cs
+++ JuegoNave/JuegoNave/Assets/Game/Scripts/UI_Manager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void playgame()
     {
+        SceneManager.LoadScene("");
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void abandonar()
     {
-        
+    Application.Quit();
     }
+
 }
+
+
